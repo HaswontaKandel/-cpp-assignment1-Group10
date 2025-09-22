09ea1a8da381d81cc862f87bd3507b98a26fd990
Hemant <hemantkandel60@gmail.com>
2025-09-23 01:12:11 +0545

    Create 5-lines-of-texts.cpp

---
+++ 5-lines-of-texts.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout<< "Line 1: Lev Nikolayevich Tolstoy was a Russian writer" << endl;
+    std::cout<< "Line 2: He is regarded as one of the greatest and most influential authors of all time." << endl;
+    std::cout<< "Line 3: Born to an aristocratic family, Tolstoy achieved acclaim in his twenties." << endl;
+    std::cout<< "Line 4: With his semi-autobiographical trilogy, Childhood, Boyhood and Youth." << endl;
+    std::cout<< "Line 5: And one of his best, based on his experiences in the Crimean War, War and Peace." << endl;
+    return 0;
+}
