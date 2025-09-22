864532f15f5eb6b41f0a3da7e19a1467056ab738
Hemant <hemantkandel60@gmail.com>
2025-09-23 01:22:07 +0545

    Create Assignment-info.cpp

---
+++ Assignment-info.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Assignment Number: 1" << endl;
+    cout << "Group Number: 10" << endl;
+    cout << "Group Member 1: Haswonta Kandel" << endl;
+    cout << "Group Member 2: Salon Subedi" << endl;
+    cout << "Group Member 3: Simanta Bhattarai" << endl;
+    return 0;
+}
